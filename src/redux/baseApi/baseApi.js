0d8850b277272d51033ad6b919de4a65b9cb968a
mahfujalam1 <mahfujahmad424@gmail.com>
2026-01-26 18:02:02 +0600

    base url change

--- src/redux/baseApi/baseApi.js
+++ src/redux/baseApi/baseApi.js
@@ -4,7 +4,7 @@ import { tagTypesList } from "../tagTypes";
 export const baseApi = createApi({
   reducerPath: "Faberge",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://x91h36px-5137.inc1.devtunnels.ms`,
+    baseUrl: `${import.meta.env.VITE_REACT_APP_BASE_URL}`,
     prepareHeaders: (headers) => {
       // Retrieve the token from your store or local storage
       const token = localStorage.getItem("token");
